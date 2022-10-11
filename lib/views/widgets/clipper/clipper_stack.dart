ca1184818790180dca427c5e7c991d48db8f7c38
ManhNTX <tmnguyen@linagora.com>
2022-10-11 14:48:01 +0700

    TF-1034:apply ui select text style for tablet

---
+++ lib/views/widgets/clipper/clipper_stack.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class ClipperStack extends CustomClipper<Path> {
+  ClipperStack({this.borderRadius = 16});
+
+  final double borderRadius;
+
+  @override
+  Path getClip(Size size) {
+    double width = size.width;
+    double height = size.height;
+    double rheight = height - 15;
+
+    final path = Path()
+      ..lineTo(0, rheight - borderRadius)
+      ..cubicTo(0, rheight - borderRadius, 0, rheight, borderRadius, rheight)
+      ..lineTo(30, rheight)
+      ..lineTo(40, height)
+      ..lineTo(50, rheight)
+      ..lineTo(width - borderRadius, rheight)
+      ..cubicTo(width - borderRadius, rheight, width, rheight, width,
+          rheight - borderRadius)
+      ..lineTo(width, 0)
+      ..lineTo(0, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
+
+@immutable
+class ClipShadowPath extends StatelessWidget {
+  final Shadow shadow;
+  final CustomClipper<Path> clipper;
+  final Widget child;
+
+  const ClipShadowPath({
+    Key? key,
+    required this.shadow,
+    required this.clipper,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      key: UniqueKey(),
+      painter: _ClipShadowShadowPainter(
+        clipper: clipper,
+        shadow: shadow,
+      ),
+      child: ClipPath(clipper: clipper, child: child),
+    );
+  }
+}
+
+class _ClipShadowShadowPainter extends CustomPainter {
+  final Shadow shadow;
+  final CustomClipper<Path> clipper;
+
+  _ClipShadowShadowPainter({required this.shadow, required this.clipper});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = shadow.toPaint();
+    var clipPath = clipper.getClip(size).shift(shadow.offset);
+    canvas.drawPath(clipPath, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
