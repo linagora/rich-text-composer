3a801e885e0beb5c1d5dfccaaf660b1385eb7b73
dab246 <tdvu@linagora.com>
2022-10-19 10:20:03 +0700

    Add DialogUtils help manage show dialog bottom sheet or alert dialog

---
+++ lib/views/widgets/dialog/dialog_utils.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+
+class DialogUtils {
+
+  static void showDialogBottomSheet(BuildContext context, Widget child) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.circular(16)),
+      ),
+      barrierColor: Colors.black38,
+      backgroundColor: Colors.white,
+      constraints: const BoxConstraints(maxWidth: 716),
+      builder: (_) {
+        return LayoutBuilder(
+            builder: (context, _) {
+              return AnimatedPadding(
+                  padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+                  duration: const Duration(milliseconds: 150),
+                  curve: Curves.easeOut,
+                  child: child
+              );
+            }
+        );
+      }
+    );
+  }
+
+  static void showDialogCenter(BuildContext context, Widget child) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          contentPadding: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          content: SizedBox(
+            width: 448,
+            height: 436,
+            child: child,
+          ),
+        );
+      },
+    );
+  }
+}
