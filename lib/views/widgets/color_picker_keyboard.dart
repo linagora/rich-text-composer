acccf912b5ebf94eafd431bab9a5936f3d9c5d05
dab246 <tdvu@linagora.com>
2024-01-24 11:42:23 +0700

    Fix format styles on tablet

--- lib/views/widgets/color_picker_keyboard.dart
+++ lib/views/widgets/color_picker_keyboard.dart
@@ -4,29 +4,39 @@ import 'package:rich_text_composer/views/commons/colors.dart';
 class ColorPickerKeyboard extends StatelessWidget {
   final Function(Color)? onSelected;
   final Color currentColor;
+  final EdgeInsetsGeometry? padding;
 
   const ColorPickerKeyboard({
     required this.currentColor,
     Key? key,
     this.onSelected,
+    this.padding,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraint) {
-      return Container(
+    return SingleChildScrollView(
+      child: Container(
         decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(4)),
-            color: Colors.white
-        ),
+          borderRadius: BorderRadius.all(Radius.circular(4)),
+          color: Colors.white),
+        padding: padding ?? const EdgeInsetsDirectional.only(
+          start: 24,
+          end: 24,
+          top: 24,
+          bottom: 35),
         child: ClipRRect(
           borderRadius: const BorderRadius.all(Radius.circular(4)),
-          child: Wrap(children: CommonColor.listColorsPicker
-              .map((color) => _itemColorWidget(context, constraint.maxWidth, color))
-              .toList()),
+          child: LayoutBuilder(builder: (context, constraint) {
+            return ClipRRect(
+              child: Wrap(children: CommonColor.listColorsPicker
+                .map((color) => _itemColorWidget(context, constraint.maxWidth, color))
+                .toList()),
+            );
+          }),
         ),
-      );
-    });
+      ),
+    );
   }
 
   Widget _itemColorWidget(BuildContext context, double maxWidth, Color color) {
--- lib/views/widgets/list_format_color.dart
+++ lib/views/widgets/list_format_color.dart
@@ -17,12 +17,16 @@ class ListFormatColor extends StatelessWidget {
   final RichTextController richTextController;
   final String? foregroundColorLabel;
   final String? backgroundColorLabel;
+  final VoidCallback? onSelectForegroundColor;
+  final VoidCallback? onSelectBackgroundColor;
 
   const ListFormatColor({
     super.key,
     required this.richTextController,
     this.foregroundColorLabel,
     this.backgroundColorLabel,
+    this.onSelectForegroundColor,
+    this.onSelectBackgroundColor,
   });
 
   @override
@@ -39,7 +43,13 @@ class ListFormatColor extends StatelessWidget {
                     key: const Key('foreground_color_button'),
                     iconAsset: ImagePaths().icTextColor,
                     iconColor: richTextController.selectedTextColor.value,
-                    onTapAction: () => _handleSelectForegroundColorAction(context),
+                    onTapAction: () {
+                      if (ResponsiveUtils().isMobile(context)) {
+                        _handleSelectForegroundColorAction(context);
+                      } else {
+                        onSelectForegroundColor?.call();
+                      }
+                    },
                     packageName: packageName,
                   ),
                 );
@@ -55,7 +65,13 @@ class ListFormatColor extends StatelessWidget {
                     iconColor: richTextController.selectedTextBackgroundColor.value == Colors.white
                       ? CommonColor.colorIconSelect
                       : richTextController.selectedTextBackgroundColor.value,
-                    onTapAction: () => _handleSelectBackgroundColorAction(context),
+                    onTapAction: () {
+                      if (ResponsiveUtils().isMobile(context)) {
+                        _handleSelectBackgroundColorAction(context);
+                      } else {
+                        onSelectBackgroundColor?.call();
+                      }
+                    },
                     packageName: packageName,
                   ),
                 );
@@ -66,50 +82,44 @@ class ListFormatColor extends StatelessWidget {
   }
 
   void _handleSelectForegroundColorAction(BuildContext context) {
-    if (ResponsiveUtils().isMobile(context)) {
-      DialogUtils().showDialogBottomSheet(
-        context,
-        ValueListenableBuilder(
+    DialogUtils().showDialogBottomSheet(
+      context,
+      OptionBottomSheet(
+        title: foregroundColorLabel ?? 'Foreground',
+        child: ValueListenableBuilder(
           valueListenable: richTextController.selectedTextColor,
           builder: (context, _, __) {
-            return OptionBottomSheet(
-              title: foregroundColorLabel ?? 'Foreground',
-              child: ColorPickerKeyboard(
-                currentColor: richTextController.selectedTextColor.value,
-                onSelected: (color) {
-                  richTextController.selectTextColor(color);
-                  Navigator.of(context).pop();
-                },
-              ),
+            return ColorPickerKeyboard(
+              currentColor: richTextController.selectedTextColor.value,
+              padding: const EdgeInsets.all(24),
+              onSelected: (color) {
+                richTextController.selectTextColor(color);
+                Navigator.of(context).pop();
+              },
             );
           }
-        ));
-    } else {
-      richTextController.currentIndexStackOverlayRichTextForTablet.value = 1;
-    }
+        ),
+      ));
   }
 
   void _handleSelectBackgroundColorAction(BuildContext context) {
-    if (ResponsiveUtils().isMobile(context)) {
-      DialogUtils().showDialogBottomSheet(
-        context,
-        ValueListenableBuilder(
+    DialogUtils().showDialogBottomSheet(
+      context,
+      OptionBottomSheet(
+        title: backgroundColorLabel ?? 'Background',
+        child: ValueListenableBuilder(
           valueListenable: richTextController.selectedTextBackgroundColor,
           builder: (context, _, __) {
-            return OptionBottomSheet(
-              title: backgroundColorLabel ?? 'Background',
-              child: ColorPickerKeyboard(
-                currentColor: richTextController.selectedTextBackgroundColor.value,
-                onSelected: (color) {
-                  richTextController.selectBackgroundColor(color);
-                  Navigator.of(context).pop();
-                },
-              )
+            return ColorPickerKeyboard(
+              currentColor: richTextController.selectedTextBackgroundColor.value,
+              padding: const EdgeInsets.all(24),
+              onSelected: (color) {
+                richTextController.selectBackgroundColor(color);
+                Navigator.of(context).pop();
+              },
             );
           }
-        ));
-    } else {
-      richTextController.currentIndexStackOverlayRichTextForTablet.value = 2;
-    }
+        ),
+      ));
   }
 }
